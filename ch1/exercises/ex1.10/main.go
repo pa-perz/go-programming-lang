68f72331d6dd224731a15e25fe4d911fa3b6b53f
Pablo Pérez <pa_perz@outlook.es>
2021-08-03 23:28:15 +0100

    Hi it's like and I don't want to write a message

---
+++ ch1/exercises/ex1.10/main.go
@@ -0,0 +1,44 @@
+// Exercise 1.10: Find a web site that produces a large amount of data.
+// Investigate caching by running fetchall twice in succession to see
+// whether the reported time changes much. Do you get the same content each time?
+// Modify fetchall to print its output to a file so it can be examined.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+
+	dst, err := os.Create("content.txt")
+	nbytes, err := io.Copy(dst, resp.Body)
+	resp.Body.Close() // don't leak resource
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
